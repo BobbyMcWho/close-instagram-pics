b6901eeac496d2c0fbcf411a8ec047cbd3c4b282
Bobby McDonald <bobbymcwho@gmail.com>
2019-01-16 21:09:32 -0500

    Fix bug preventing from interacting with the post.

--- close-instagram-pics/js/instagram.js
+++ close-instagram-pics/js/instagram.js
@@ -1,8 +1,10 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.isInstagramPostPage === true){
       let dialogNode = document.querySelector("div[role='dialog']");
-      dialogNode.addEventListener('click', () => {
-        window.history.back();
+      dialogNode.addEventListener('click', (e) => {
+        if(e.target !== document.querySelectorAll("div[role='dialog']")[2]){
+          window.history.back();
+        }
       });
     }
   });
